73182a89361fa5dc58e396dae6a8b6b3182933b5
Kseniia Ernest <hramtcovakseniia@gmail.com>
2024-07-05 14:48:06 -0400

    Initial commit + initial layout

---
+++ src/Main.js
@@ -0,0 +1,27 @@
+import { Component } from "react";
+
+
+export default class Main extends Component {
+
+
+  render() {
+    return(
+      <main>
+        <div>BIG + audio</div>
+        
+        <div>
+        <p>NOUN</p>
+        <div>Meaning list</div>
+        </div>
+      
+        <div>Synonyms</div>
+
+        <div>
+        <p>VERB</p>
+        <div>Meaning list</div>
+        </div>
+
+      </main>
+    )
+  }
+}
---
+++ src/Search.js
@@ -0,0 +1,11 @@
+import { Component } from "react";
+
+export default class Search extends Component {
+
+
+  render() {
+    return(
+      <input className="search-bar" type="text" placeholder="Search for any word..."/>
+    )
+  }
+}
